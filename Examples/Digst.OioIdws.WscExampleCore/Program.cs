9e4ff9fac598fdf5d6fd55cad9cee97185a03afe
Jonas Pedersen <jpe@kvalitetsit.dk>
2023-07-03 15:18:17 +0200

    Move WspAccessToken to oen class and add some comments.

--- Examples/Digst.OioIdws.WscExampleCore/Program.cs
+++ Examples/Digst.OioIdws.WscExampleCore/Program.cs
@@ -9,15 +9,9 @@ using System;
 using System.IdentityModel.Tokens;
 using System.Net.Http;
 using System.Text.Json;
-using System.Text.Json.Serialization;
 
 namespace Digst.OioIdws.WscExampleCore
 {
-    public class WspAccessToken
-    {
-        [JsonPropertyName("access_token")]
-        public string? AccessToken { get; set; }
-    }
 
     internal static class Program
     {
@@ -38,29 +32,43 @@ namespace Digst.OioIdws.WscExampleCore
                 var securityToken = (GenericXmlSecurityToken)stsTokenService.GetToken();
                 Console.WriteLine("Token: " + securityToken.TokenXml.OuterXml);
 
-                // COnfigure HTTP client to use mTLS
+                // Configure HTTP client to use mTLS
                 var httpClientHandler = new HttpClientHandler();
                 httpClientHandler.ClientCertificateOptions = ClientCertificateOption.Manual;
                 httpClientHandler.ClientCertificates.Add(stsConfiguration.ClientCertificate);
                 httpClientHandler.SslProtocols = System.Security.Authentication.SslProtocols.Tls12;
                 using (var client = new HttpClient(httpClientHandler))
                 {
-                    // Authorize
-                    var content = new StringContent("saml-token=" + System.Convert.ToBase64String(System.Text.Encoding.UTF8.GetBytes(securityToken.TokenXml.OuterXml)));
+                    // Authorize against service
+                    var assertionString = securityToken.TokenXml.OuterXml;
+                    var content = new StringContent("saml-token=" + Convert.ToBase64String(System.Text.Encoding.UTF8.GetBytes(assertionString)));
 
                     var authorizeResult = client.PostAsync("https://videoapi.vconf-stage.dk/videoapi/token", content).Result;
-                    var c = authorizeResult.Content.ReadAsStringAsync().Result;
-                    var accessToken = JsonSerializer.Deserialize<WspAccessToken>(c);
+                    if(!authorizeResult.IsSuccessStatusCode)
+                    {
+                        throw new Exception(authorizeResult.ToString());
+                    }
+
+                    // Parse response
+                    var authorizeContent = authorizeResult.Content.ReadAsStringAsync().Result;
+                    var accessToken = JsonSerializer.Deserialize<WspAccessToken>(authorizeContent);
 
 
                     // Call service
+                    // Add Auhtorization header.
                     var request = new HttpRequestMessage()
                     {
                         RequestUri = new Uri("https://videoapi.vconf-stage.dk/videoapi/meetings/b2670f63-04e0-46ec-bf61-1cbd0baa15d3"),
                     };
                     request.Headers.Add("Authorization", "Holder-of-key " +  accessToken?.AccessToken);
                     
+                    // Call service and print response
                     var serviceResult = client.SendAsync(request).Result;
+
+                    if(!serviceResult.IsSuccessStatusCode)
+                    {
+                        throw new Exception(serviceResult.ToString());
+                    }
                     Console.WriteLine(serviceResult.Content.ReadAsStringAsync().Result);
                 }
             }
---
+++ Examples/Digst.OioIdws.WscExampleCore/WspAccessToken.cs
@@ -0,0 +1,24 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Digst.OioIdws.WscExampleCore
+{
+    public class WspAccessToken
+    {
+        [JsonPropertyName("access_token")]
+        /// <summary>
+        /// Access token to use in authorization header in service calls.
+        /// </summary>
+        public string? AccessToken { get; set; }
+        [JsonPropertyName("expires_in")]
+        /// <summary>
+        /// Seconds before this token expires and new one must be created. This is seconds from when token was initially created.
+        /// </summary>
+        public int ExpiresIn { get; set; }
+
+        [JsonPropertyName("token_type")]
+        /// <summary>
+        /// Type of token. Either holder-of-key or bearer.
+        /// </summary>
+        public string? TokenType { get; set; }
+    }
+}
